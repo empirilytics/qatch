1df08728a5e03231c0ac22f6c8e5005f160e6a0a
Isaac Griffith <isaacdgriffith@gmail.com>
2022-07-21 08:56:04 -0600

    added some comments

--- analyzers/src/main/java/com/empirilytics/qatch/analyzers/python/PylintAnalyzer.java
+++ analyzers/src/main/java/com/empirilytics/qatch/analyzers/python/PylintAnalyzer.java
@@ -10,18 +10,20 @@ import org.jetbrains.annotations.NotNull;
  */
 public class PylintAnalyzer implements IssuesAnalyzer {
 
-    public PylintAnalyzer(String pylintPath, String resultsPath, String ruleSetPath) {
+  public PylintAnalyzer(String pylintPath, String resultsPath, String ruleSetPath) {}
 
-    }
+  public void analyze(
+      @NotNull String src,
+      @NotNull String dest,
+      @NotNull String ruleset,
+      @NotNull String filename) {
 
-    public void analyze(@NotNull String src, @NotNull String dest, @NotNull String ruleset, @NotNull String filename) {
-
+    // need to set working directory to python directory
+    // pylint --recursive=y --rcfile=ruleset --output-format=json -j 4 filename filename2 filename3 ...
   }
 
-	//Alternative
-	//public abstract void analyze(String src, String dest, String ruleset, String filename);
+  // Alternative
+  // public abstract void analyze(String src, String dest, String ruleset, String filename);
 
-	public void analyze(@NotNull String src, @NotNull String dest, @NotNull PropertySet properties) {
-
-  }
+  public void analyze(@NotNull String src, @NotNull String dest, @NotNull PropertySet properties) {}
 }
--- analyzers/src/main/java/com/empirilytics/qatch/analyzers/python/PylintResultsImporter.java
+++ analyzers/src/main/java/com/empirilytics/qatch/analyzers/python/PylintResultsImporter.java
@@ -4,12 +4,18 @@ import com.empirilytics.qatch.analyzers.IssuesImporter;
 import com.empirilytics.qatch.core.eval.IssueSet;
 
 /**
+ * Imports the results of a pylint execution
+ *
  * @author Isaac Griffith
  * @version 2.0.0
  */
 public class PylintResultsImporter implements IssuesImporter {
 
+    /** {@inheritDoc} */
+    @Override
     public IssueSet parseIssues(String path) {
+        // Apparently messages come in the following
+
         return null;
     }
 }
